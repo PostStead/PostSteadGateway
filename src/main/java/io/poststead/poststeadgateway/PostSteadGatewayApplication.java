7e59acb5201d1047ec3bf55c4f8365bffb55b243
Alecsandu <alecsandu678@gmail.com>
2022-12-26 22:08:26 +0200

    info: Initial commit.
    Added routing of the users and posts microservices along with a basic Dockerfile.

---
+++ src/main/java/io/poststead/poststeadgateway/PostSteadGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.poststead.poststeadgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostSteadGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostSteadGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/poststead/poststeadgateway/PostSteadGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.poststead.poststeadgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostSteadGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
